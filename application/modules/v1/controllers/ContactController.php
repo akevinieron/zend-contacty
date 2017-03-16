d7e350398f3ccf504890aeb5ab46bbcfb55b288e
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2017-03-16 06:11:53 -0400

    Implementing methods

--- application/modules/v1/controllers/ContactController.php
+++ application/modules/v1/controllers/ContactController.php
@@ -4,11 +4,14 @@ class ContactController extends Zend_Rest_Controller
 {
 
     private $alegraClient;
+    private $httpClient;
 
     public function init()
     {
         $this->_helper->viewRenderer->setNoRender(true);
         $this->alegraClient = new Zend_Config(require dirname(__FILE__) . '/../configs/alegra.php');
+        $this->httpClient = new Zend_Http_Client();
+        $this->httpClient->setAuth($this->alegraClient->email, $this->alegraClient->apiKey);
     }
 
     public function headAction()
@@ -16,62 +19,85 @@ class ContactController extends Zend_Rest_Controller
     
     public function indexAction()
     {
+        $this->httpClient->setUri("https://app.alegra.com/api/v1/contacts/");
+
+        $response = $this->httpClient->request('GET');
         $this->getResponse()->setHttpResponseCode(200);
-        
-        $data = [
-            "data" => null,
+
+        $fullResponse = [
+            "data" => Zend_Json::decode($response->getBody()),
             "message" => "Success"
         ];
 
-        $this->_helper->json($data);
+        $this->_helper->json($fullResponse);
     }
 
     public function getAction()
     {
-      $this->getResponse()->setHttpResponseCode(200);
-        
-        $data = [
-            "data" => null,
+        $idParam = $this->_getParam('id');
+        $this->httpClient->setUri("https://app.alegra.com/api/v1/contacts/" . $idParam);
+
+        $response = $this->httpClient->request('GET');
+        $this->getResponse()->setHttpResponseCode(200);
+
+        $fullResponse = [
+            "data" => Zend_Json::decode($response->getBody()),
             "message" => "Success"
         ];
 
-        $this->_helper->json($data);
+        $this->_helper->json($fullResponse);
     }
 
     public function postAction()
     {
-      $this->getResponse()->setHttpResponseCode(200);
-        
-        $data = [
-            "data" => null,
+        $this->httpClient->setUri("https://app.alegra.com/api/v1/contacts/");
+
+        $data = $this->getRequest()->getRawBody();
+        $this->httpClient->setRawData($data, "application/json");
+
+        $response = $this->httpClient->request('POST');
+        $this->getResponse()->setHttpResponseCode(200);
+
+        $fullResponse = [
+            "data" => Zend_Json::decode($response->getBody()),
             "message" => "Success"
         ];
 
-        $this->_helper->json($data);
+        $this->_helper->json($fullResponse);
     }
 
     public function putAction()
     {
-      $this->getResponse()->setHttpResponseCode(200);
-        
-        $data = [
-            "data" => null,
+        $this->httpClient->setUri("https://app.alegra.com/api/v1/contacts/");
+
+        $data = $this->getRequest()->getRawBody();
+        $this->httpClient->setRawData($data, "application/json");
+
+        $response = $this->httpClient->request('PUT');
+        $this->getResponse()->setHttpResponseCode(200);
+
+        $fullResponse = [
+            "data" => Zend_Json::decode($response->getBody()),
             "message" => "Success"
         ];
 
-        $this->_helper->json($data);
+        $this->_helper->json($fullResponse);
     }
 
     public function deleteAction()
     {
-      $this->getResponse()->setHttpResponseCode(200);
-        
-        $data = [
-            "data" => null,
+        $idParam = $this->_getParam('id');
+        $this->httpClient->setUri("https://app.alegra.com/api/v1/contacts/" . $idParam);
+
+        $response = $this->httpClient->request('DELETE');
+        $this->getResponse()->setHttpResponseCode(200);
+
+        $fullResponse = [
+            "data" => Zend_Json::decode($response->getBody()),
             "message" => "Success"
         ];
 
-        $this->_helper->json($data);
+        $this->_helper->json($fullResponse);
     }
 
 }
