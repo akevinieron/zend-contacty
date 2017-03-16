922d89e0fcd9b20df88a234012370074fc9c4716
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2017-03-16 05:02:54 -0400

    Adding config data to controller and boilerplate response on each method

--- application/modules/v1/controllers/ContactController.php
+++ application/modules/v1/controllers/ContactController.php
@@ -3,28 +3,76 @@
 class ContactController extends Zend_Rest_Controller
 {
 
+    private $alegraClient;
+
     public function init()
     {
         $this->_helper->viewRenderer->setNoRender(true);
+        $this->alegraClient = new Zend_Config(require dirname(__FILE__) . '/../configs/alegra.php');
     }
 
     public function headAction()
     {}
     
     public function indexAction()
-    {}
+    {
+        $this->getResponse()->setHttpResponseCode(200);
+        
+        $data = [
+            "data" => null,
+            "message" => "Success"
+        ];
+
+        $this->_helper->json($data);
+    }
 
     public function getAction()
-    {}
+    {
+      $this->getResponse()->setHttpResponseCode(200);
+        
+        $data = [
+            "data" => null,
+            "message" => "Success"
+        ];
+
+        $this->_helper->json($data);
+    }
 
     public function postAction()
-    {}
+    {
+      $this->getResponse()->setHttpResponseCode(200);
+        
+        $data = [
+            "data" => null,
+            "message" => "Success"
+        ];
+
+        $this->_helper->json($data);
+    }
 
     public function putAction()
-    {}
+    {
+      $this->getResponse()->setHttpResponseCode(200);
+        
+        $data = [
+            "data" => null,
+            "message" => "Success"
+        ];
+
+        $this->_helper->json($data);
+    }
 
     public function deleteAction()
-    {}
+    {
+      $this->getResponse()->setHttpResponseCode(200);
+        
+        $data = [
+            "data" => null,
+            "message" => "Success"
+        ];
+
+        $this->_helper->json($data);
+    }
 
 }
 
