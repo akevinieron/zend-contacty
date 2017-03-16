4cb0f7d6f7695a00330c9887bd32e464a04eca9d
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2017-03-16 02:03:43 -0400

    Adding v1 module and Contact Controller with REST methods

---
+++ application/modules/v1/controllers/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+
+class ContactController extends Zend_Rest_Controller
+{
+
+    public function init()
+    {
+        $this->_helper->viewRenderer->setNoRender(true);
+    }
+
+    public function headAction()
+    {}
+    
+    public function indexAction()
+    {}
+
+    public function getAction()
+    {}
+
+    public function postAction()
+    {}
+
+    public function putAction()
+    {}
+
+    public function deleteAction()
+    {}
+
+}
+
